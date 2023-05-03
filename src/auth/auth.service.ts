de276bdf719a75f05cfc704aef504dcc44a8bb24
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-05-03 19:32:06 +0200

    feat(singup): hash password, add user to db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -8,6 +9,24 @@ export class AuthService {
 
   async signup(dto: AuthDto) {
     const { email, password } = dto;
+
+    const foundUser = await this.prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (foundUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    await this.prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+      },
+    });
+
     return { message: 'signup message' };
   }
 
@@ -17,5 +36,12 @@ export class AuthService {
 
   async singout() {
     return '';
+  }
+
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+
+    return hashedPassword;
   }
 }
